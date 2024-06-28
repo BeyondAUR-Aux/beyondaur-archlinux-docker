99631cbca7fb00af9f996b0009c8b214a6168e47
evan0greenup <44539531+evan0greenup@users.noreply.github.com>
2024-06-29 01:54:05 +1000

    good

---
+++ config-archlinux.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+
+# Enable the multilib repository
+cat << EOM >> /etc/pacman.conf
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+
+[beyondaur]
+SigLevel = Optional
+Server = https://beyondaur.github.io/__repo__/\$arch
+EOM
+
+pacman -Syu --noconfirm --needed base-devel hx-ghcup-hs libyaml libffi
+
+
+ghcup set stack latest
+ghcup set ghc latest
+ghcup set cabal latest
+
