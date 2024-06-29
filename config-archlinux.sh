df2ccfa88bc5857e5d44a8b1227fd52e358a0e21
evan0greenup <44539531+evan0greenup@users.noreply.github.com>
2024-06-29 10:11:39 +1000

    Update config-archlinux.sh

--- config-archlinux.sh
+++ config-archlinux.sh
@@ -3,9 +3,6 @@
 
 # Enable the multilib repository
 cat << EOM >> /etc/pacman.conf
-[multilib]
-Include = /etc/pacman.d/mirrorlist
-
 [beyondaur]
 SigLevel = Optional
 Server = https://beyondaur.github.io/__repo__/\$arch
